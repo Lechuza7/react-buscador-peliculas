e1eea607cb29676e6ad696653ced5292b8d2b20a
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-05-15 20:45:27 +0200

    Form control + API fetch as a service

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies.jsx";
 import { useMovies } from "./hooks/useMovies.js";
 
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    if (search === '') {
+      setError('No se puede hacer una búsqueda vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se pueden hacer búsquedas númericas')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener almenos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
 function App() {
-  const { movies: mappedMovies } = useMovies();
-  const [query, setQuery] = useState("");
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies } = useMovies({ search });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const fields = new window.FormData(event.target);
-    const query = fields.get("query");
-    console.log(query);
+    getMovies()
   };
 
   const handleChange = (event) => {
-    setQuery(event.target.value);
+    updateSearch(event.target.value);
   };
+
+
 
   return (
     <div className="page">
@@ -24,17 +50,22 @@ function App() {
         <h1>Buscador de películas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }}
             onChange={handleChange}
-            value={query}
-            name="query"
+            value={search}
+            name="search"
             placeholder="Big Fish, Lock & Stock, Star Wars..."
           />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
