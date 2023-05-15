3884c249f1acbf6bfe0a78af4339a6ac907e5498
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-05-15 18:15:26 +0200

    useState query, handleSubmit, handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import './App.css'
-import responseMovies from './mocks/with-results.json'
-import { Movies } from './components/Movies'
+import { useState } from "react";
+import "./App.css";
+import { Movies } from "./components/Movies.jsx";
+import { useMovies } from "./hooks/useMovies.js";
 
 function App() {
-  const movies = responseMovies.Search
+  const { movies: mappedMovies } = useMovies();
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fields = new window.FormData(event.target);
+    const query = fields.get("query");
+    console.log(query);
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
 
   return (
-    <div className='page'>
-
+    <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Big Fish, Lock & Stock, Star Wars...'/>
-          <button type='submit'>Buscar</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={query}
+            name="query"
+            placeholder="Big Fish, Lock & Stock, Star Wars..."
+          />
+          <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={movies} />
+        <Movies movies={mappedMovies} />
       </main>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,31 +1,24 @@
-//import withoutResult from './mocks/no-results.json'
-
 function ListOfMovies({ movies }) {
+  
   return (
     <ul>
       {movies.map((movie) => (
-        <li key={movie.imdbID}>
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} />
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title} />
         </li>
       ))}
     </ul>
-  );
-}
-
-function NoMoviesResults () {
-  return (
-    <p>Búsqueda sin resultados</p>
   )
 }
 
-export function Movies ({ movies }) {
-  const hasMovies = movies?.length > 0
+function NoMoviesResults() {
+  return <p>Búsqueda sin resultados</p>;
+}
 
-  return (
-    hasMovies
-    ? <ListOfMovies movies={movies} />
-    : <NoMoviesResults />
-  )
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />;
 }
