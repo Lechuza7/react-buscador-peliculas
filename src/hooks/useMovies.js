0c4914a7a4fbdaf34d55c4e7f78193b45894c6fe
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-05-18 18:57:34 +0200

    Custom hook useMovies

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies.js'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  //usaremos useRef para almacenar el valor de la búsqueda previa entre renderizados e impedir con ese dato repetir dicha búsqueda
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
-    const newMovies = await searchMovies({ search })
-    setMovies(newMovies)
+    if (search === previousSearch.current) return
+
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      //finally se ejecuta tanto si entre el try como si va por catch
+      setLoading(false)
+    }
+    
   }
 
-  return { movies, getMovies }
+  return { movies, loading, getMovies, error }
 }
