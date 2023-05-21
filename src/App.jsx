bfb6908b595acb3385fa8c56d90da394014b9ea0
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-05-21 12:14:46 +0200

    Debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,64 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies.jsx";
 import { useMovies } from "./hooks/useMovies.js";
+import debounce from "just-debounce-it";
 
-function useSearch () {
-  const [search, updateSearch] = useState('')
-  const [error, setError] = useState(null)
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (search === '') {
-      setError('No se puede hacer una búsqueda vacía')
-      return
+    if (search === "") {
+      setError("No se puede hacer una búsqueda vacía");
+      return;
     }
 
     if (search.match(/^\d+$/)) {
-      setError('No se pueden hacer búsquedas númericas')
-      return
+      setError("No se pueden hacer búsquedas númericas");
+      return;
     }
 
     if (search.length < 3) {
-      setError('La búsqueda debe tener almenos 3 caracteres')
-      return
+      setError("La búsqueda debe tener almenos 3 caracteres");
+      return;
     }
 
-    setError(null)
-  }, [search])
+    setError(null);
+  }, [search]);
 
-  return { search, updateSearch, error }
+  return { search, updateSearch, error };
 }
 
 function App() {
-  const [sort, setSort] = useState(false)
+  const [sort, setSort] = useState(false);
 
   const { search, updateSearch, error } = useSearch();
   const { movies, loading, getMovies } = useMovies({ search, sort });
 
+  // Usamos debounce con callback para que no se cree de nuevo en cada renderizado.
+  // 300milisegundos es un buen estándar, un usuario suele tardar entre 100-250 en teclear.
+  const debouncedGetMovies = useCallback(
+    debounce((search) => {
+      getMovies({ search });
+    }, 300),
+    []
+  );
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    getMovies({ search })
+    getMovies({ search });
   };
 
   const handleSort = () => {
-    setSort(!sort)
-  }
-
-  const handleChange = (event) => {
-    const newSearch = event.target.value
-    updateSearch(newSearch);
-    getMovies({ search: newSearch })
+    setSort(!sort);
   };
 
-
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+    debouncedGetMovies(newSearch);
+  };
 
   return (
     <div className="page">
@@ -59,26 +67,21 @@ function App() {
         <form className="form" onSubmit={handleSubmit}>
           <input
             style={{
-              border: '1px solid transparent',
-              borderColor: error ? 'red' : 'transparent'
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
             }}
             onChange={handleChange}
             value={search}
             name="search"
             placeholder="Big Fish, Lock & Stock, Star Wars..."
           />
-          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type="submit">Buscar</button>
         </form>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
-      <main>
-        {
-          loading ? <p>Cargando...</p> : <Movies movies={movies} />
-        }
-        
-      </main>
+      <main>{loading ? <p>Cargando...</p> : <Movies movies={movies} />}</main>
     </div>
   );
 }
